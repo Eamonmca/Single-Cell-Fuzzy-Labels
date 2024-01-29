2fc4357735fc6b714b3ebd5b9c62cfe14dbbaa7f
Eamon <mcandrew@bego.ipmc.cnrs.fr>
2024-01-29 14:32:20 +0100

    Testing Docs

--- Single_Cell_Fuzzy_Labels/_modidx.py
+++ Single_Cell_Fuzzy_Labels/_modidx.py
@@ -5,4 +5,12 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Eamonmca.github.io',
                 'git_url': 'https://github.com/Eamonmca/Single-Cell-Fuzzy-Labels',
                 'lib_path': 'Single_Cell_Fuzzy_Labels'},
-  'syms': {'Single_Cell_Fuzzy_Labels.core': {'Single_Cell_Fuzzy_Labels.core.foo': ('core.html#foo', 'Single_Cell_Fuzzy_Labels/core.py')}}}
+  'syms': { 'Single_Cell_Fuzzy_Labels.KNN_Label_transfer': { 'Single_Cell_Fuzzy_Labels.KNN_Label_transfer.knn_majority_voting': ( 'knn_label_transfer.html#knn_majority_voting',
+                                                                                                                                  'Single_Cell_Fuzzy_Labels/KNN_Label_transfer.py'),
+                                                             'Single_Cell_Fuzzy_Labels.KNN_Label_transfer.knn_weighted_voting': ( 'knn_label_transfer.html#knn_weighted_voting',
+                                                                                                                                  'Single_Cell_Fuzzy_Labels/KNN_Label_transfer.py'),
+                                                             'Single_Cell_Fuzzy_Labels.KNN_Label_transfer.run_label_transfer': ( 'knn_label_transfer.html#run_label_transfer',
+                                                                                                                                 'Single_Cell_Fuzzy_Labels/KNN_Label_transfer.py')},
+            'Single_Cell_Fuzzy_Labels.census': { 'Single_Cell_Fuzzy_Labels.census.download_and_embed_census_data': ( 'download_embeddings_census.html#download_and_embed_census_data',
+                                                                                                                     'Single_Cell_Fuzzy_Labels/census.py')},
+            'Single_Cell_Fuzzy_Labels.core': {'Single_Cell_Fuzzy_Labels.core.foo': ('core.html#foo', 'Single_Cell_Fuzzy_Labels/core.py')}}}
